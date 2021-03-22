69f523774ef1dea3b18569c41aee0b79ff8edcc5
tekkay <wellingtontekay@gmail.com>
2021-03-22 20:33:31 -0300

    pronto

---
+++ appTodo_webII/src/main/java/br/edu/unipam/entity/Usuario.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unipam.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author cueio
+ */
+@Entity
+@NamedQuery(name =  Usuario.GET_ALL_USERS, query = "select u from Usuario u order by u.nome")
+public class Usuario extends AbstractEntity {
+
+    public static final String GET_ALL_USERS = "Usuario.getAllUsers";
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String email;
+
+    @OneToMany(mappedBy = "usuario")
+    private Collection<Tarefa> tarefas;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String Email) {
+        this.email = Email;
+    }
+
+   
+    
+}
