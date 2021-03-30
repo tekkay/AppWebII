36ade5e6b8866858671dc6c6c269b323879bb07f
tekkay <wellingtontekay@gmail.com>
2021-03-29 22:56:49 -0300

    Correção

--- appTodo_webII/src/main/java/br/edu/unipam/resource/TarefaResource.java
+++ appTodo_webII/src/main/java/br/edu/unipam/resource/TarefaResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.unipam.resource;
 
 import br.edu.unipam.entity.Tarefa;
@@ -11,36 +6,26 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-/**
- *
- * @author Cueio
- */
-@Path("tarefa") //domínio/contextPath/api/v1/tarefa
+@Path("tarefa")
 public class TarefaResource {
 
     @Inject
     private TarefaService tarefaService;
 
     @GET
-    @Path("list") //domínio/contextPath/api/v1/tarefa/list
-    public List<Tarefa> listarTodos()
-    {
+    @Path("list")
+    public Response listar() {
         List<Tarefa> tarefaList = tarefaService.listarTodos();
-        return tarefaList;
+        return Response.ok(tarefaList).build();
     }
 
     @GET
-    @Path("user_list") //domínio/contextPath/api/v1/tarefa/user_list
-    public List<Tarefa> listarPorUsuario (Long id)
-    {
-        Usuario user = usuarioService.localizarPorId(id);
-        return entityManager.createQuery(
-                "select t from Tarefa t where t.usuario = :user", Tarefa.class)
-                .setParameter("user", user)
-                .getResultList();
-        return ResultList;
+    @Path("listByUser/{id}")
+    public Response listarPorUsuario(@PathParam("id") Long id) {
+        List<Tarefa> tarefaList = tarefaService.listarPorUsuario(id);
+        return Response.ok(tarefaList).build();
     }
-
 }
